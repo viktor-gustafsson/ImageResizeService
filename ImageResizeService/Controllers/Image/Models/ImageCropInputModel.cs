ba5a3c25fb19e9c6197440352148d23a78b99ba4
Viktor Gustafsson <viktor.gustafsson@bannerflow.com>
2019-03-22 22:09:08 +0100

    implements cropping part of the api

--- ImageResizeService/Controllers/Image/Models/ImageCropInputModel.cs
+++ ImageResizeService/Controllers/Image/Models/ImageCropInputModel.cs
@@ -9,25 +9,24 @@ namespace ImageResizeService.Controllers.Image.Models
     {
         [Required]
         [SizeValidator]
-        [FromQuery(Name = "x")]
-        public int X { get; set; }
+        [FromQuery(Name = "left")]
+        public int Left { get; set; }
 
         [Required]
         [SizeValidator]
-        [FromQuery(Name = "y")]
-        public int Y { get; set; }
+        [FromQuery(Name = "top")]
+        public int Top { get; set; }
         
         [Required]
         [SizeValidator]
-        [FromQuery(Name = "widthtocrop")]
-        public int WidthToCrop { get; set; }   
+        [FromQuery(Name = "right")]
+        public int Right { get; set; }   
         
         [Required]
         [SizeValidator]
-        [FromQuery(Name = "heighttocrop")]
-        public int HeightToCrop { get; set; }
+        [FromQuery(Name = "bottom")]
+        public int Bottom { get; set; }
         
-        public SKPoint Point => new SKPoint(X, Y);
         public SKSize Size => new SKSize(Width, Height);
     }
 }
